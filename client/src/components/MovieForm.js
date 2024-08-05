0d0e0c83e134317142e53ff14dc7b007bb3d7d8f
tonniegeetz <tonniegeetz@gmail.com>
2024-08-05 13:06:50 +0300

    validation tings

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -15,6 +16,29 @@ function MovieForm() {
   });
 
   function handleSubmit(e) {
+
+    // // make the function async to enable the await keyword
+    // async function handleSubmit(e) {
+    //   e.preventDefault();
+    //   // fetch returns a Promise, we must await it
+    //   const response = await fetch("/movies", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(formData),
+    //   });
+    //   // response.json() returns a Promise, we must await it
+    //   const data = await response.json();
+    //   if (response.ok) {
+    //     console.log("Movie created:", data);
+    //   } else {
+    //     setErrors(data.errors);
+    //   }
+    // }
+
+
+
     e.preventDefault();
     fetch("/movies", {
       method: "POST",
@@ -23,8 +47,14 @@ function MovieForm() {
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((newMovie) => console.log(newMovie));
+        } else {
+          response.json().then((errorData) => setErrors(errorData.errors));
+        }
+      })
+    // .then((newMovie) => console.log(newMovie));
   }
 
   function handleChange(e) {
@@ -114,6 +144,13 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+        {errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
         <FormGroup>
           <label htmlFor="female_director">
             Female Director?
